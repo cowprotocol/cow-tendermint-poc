eb8d96a11557838e0ed86296c59dbf16163a26b9
Felix Leupold <felixleupold90@gmail.com>
2025-03-14 10:05:54 +0000

    avoid schedule drift

--- src/domain/schedule.ts
+++ src/domain/schedule.ts
@@ -3,7 +3,7 @@ const AUCTION_FREQUENCY_MILLIS = 10_000;
 /**
  * Schedule a job to check after every auction deadline if any solvers haven't bid and cast an empty vote for them.
  */
-export function schedule(job: (number) => void, milliseconds_before_deadline) {
+export function schedule(job: (number) => void, milliseconds_before_deadline: number) {
   const now = new Date();
   let auctionId = Math.floor(now.getTime() / AUCTION_FREQUENCY_MILLIS);
   let msUntilNextExecution =
@@ -15,10 +15,11 @@ export function schedule(job: (number) => void, milliseconds_before_deadline) {
   }
 
   setTimeout(() => {
-    job(auctionId++);
-
-    setInterval(() => {
-      job(auctionId++);
-    }, AUCTION_FREQUENCY_MILLIS);
+    step(job, milliseconds_before_deadline, auctionId++);
   }, msUntilNextExecution - milliseconds_before_deadline);
+}
+
+function step(job: (number) => void, milliseconds_before_deadline:number, auction:number ) {
+  job(auction);
+  schedule(job, milliseconds_before_deadline);
 }
